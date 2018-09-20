549b763cd9642b2a68d4ac8161ced62688281202
Wetlsd <desert1988@gmail.com>
2018-09-20 12:30:47 +0000

    add started routes in  app.js file, also npm install express and ejs for future reasons=)

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var express = require("express");
+var app = express();
+
+
+app.get("/", function(req, res){
+    res.send("So let's start our trip!");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("App is running well!");
+});
