b2d29760a3e96fcaba3a29ad95d6682d3b4e3dd0
Wetlsd <desert1988@gmail.com>
2018-09-20 12:52:02 +0000

    made folder "views" for ".ejs" files, add file in views/landigpage.ejs, also in app.js setup engine ejs and rendering landingpage.ejs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
 
+//below is activation of "views" folder  with ".ejs" files inside, so from now you can rendering that files
+app.set("view engine", "ejs"); 
 
 app.get("/", function(req, res){
-    res.send("So let's start our trip!");
+    // <=== randering a file views/landingpage.ejs
+    res.render("landingpage");
+    
 });
 
 
@@ -11,3 +15,4 @@ app.get("/", function(req, res){
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("App is running well!");
 });
+
