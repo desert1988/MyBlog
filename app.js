a1d87f7cc1d31b5d4878f6030f5e71858db49954
Wetlsd <desert1988@gmail.com>
2018-10-26 10:42:59 +0000

    Add mogoose(install, add schema and setup  model in  app.js file); connect project to mongoDB and create guide database; change routes

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+const express = require("express"),
+      app = express(),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose");
 
+//connect withthing mongoose to guide DB(mongoDB)
+mongoose.connect("mongodb://localhost/guide");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 //below is activation of "views" folder  with ".ejs" files inside, so from now you can rendering that files
 app.set("view engine", "ejs"); 
 
-var places = [
-        {name: "Shenzhen", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/View_east_along_Sungang_East_Road_from_Renmin_North_Road%2C_Shenzhen%2C_China.jpg/332px-View_east_along_Sungang_East_Road_from_Renmin_North_Road%2C_Shenzhen%2C_China.jpg"},
-        {name: "Hong Kong", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Hong_kong_bruce_lee_statue.jpg/220px-Hong_kong_bruce_lee_statue.jpg"},
-        {name: "Guangzhou", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Guangzhou_skyline.jpg/261px-Guangzhou_skyline.jpg"}
-    ];
+//Schema setup for Mongo
+let placeSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+//Model setup for Mongo
+let Place = mongoose.model("Place", placeSchema);
+
+//Manual place creation (test reasons only)
+/*Place.create({
+    name: "Shenzhen", 
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/View_east_along_Sungang_East_Road_from_Renmin_North_Road%2C_Shenzhen%2C_China.jpg/332px-View_east_along_Sungang_East_Road_from_Renmin_North_Road%2C_Shenzhen%2C_China.jpg"
+    
+}, function(err, place){
+    if(err){
+        console.log("Hah you pick up an ERROR =)");
+        console.log(err);
+    } else {
+        console.log("Added new place!");
+        console.log(place);
+    } 
+});*/
+
+
 
 app.get("/", function(req, res){
     // <=== randering a file views/landingpage.ejs
@@ -21,21 +44,36 @@ app.get("/", function(req, res){
 });
 
 app.get("/places", function(req, res){
-    //places Array
-    
-    
-    res.render("places", {places: places});
+    // Get all Places from Mongo (guide DB)
+    Place.find({}, function(err, allPlaces){
+        if(err){
+            console.log("You are catch an ERROR, lol;)");
+            console.log(err);
+        } else {
+            res.render("places", {places: allPlaces});
+        }
+    });
+   
+   
+    //old places Array on server
+    // res.render("places", {places: places});
 });
 
 
 app.post("/places", function(req, res){
     // get data and add to places Array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newPlace = {name: name, image: image};
-    places.push(newPlace);
-    // redirect to places page
-    res.redirect("/places");
+    let name = req.body.name,
+        image = req.body.image,
+        newPlace = {name: name, image: image};
+    //Create a new campground and save to guide DB
+    Place.create(newPlace, function(err, placeCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect to places page
+            res.redirect("/places");
+        }
+    });
 });
 
 app.get("/places/new", function(req, res) {
