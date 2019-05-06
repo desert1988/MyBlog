aea24dc2895ce25ea7270e8ebaaf14c3493af999
Wetlsd <desert1988@gmail.com>
2019-05-06 11:05:34 +0000

    Change mongo url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const commentsRoutes = require("./routes/comments"),
 
 //connect withthing mongoose to guide DB(mongoDB)
 //mongoose.connect("mongodb://localhost/guide");
-mongoose.connect("mongodb+srv://desert:Simsim#123@cluster0-oul8i.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://desert:PASS_MONGO@cluster0-oul8i.mongodb.net/test?retryWrites=true");
 
 //body parser conf read in man
 app.use(bodyParser.urlencoded({extended: true}));
@@ -71,4 +71,5 @@ app.use("/places", placesRoutes);
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("App is running well!");
 });
+
 
